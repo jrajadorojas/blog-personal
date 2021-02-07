5dec12b9e0c44ace4ddfc617a8559d86cc481132
unknown <jrajadorojas@gmial.com>
2021-02-07 21:50:46 +0100

    Introducción a la creación del blog personal.

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+# Django
+from django.urls import path
+
+# Views
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path('',views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail')
+]
